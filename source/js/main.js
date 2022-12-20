5006310d44acff181dc741d4efe30efcb68b0954
unknown <stolyarova.199535@gmail.com>
2022-12-20 16:29:51 +0300

    главный блок и зал

--- source/js/main.js
+++ source/js/main.js
@@ -12,6 +12,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+  // gym
+
+  const gymVideo = document.querySelector('.gym__video-wrapper');
+  const gymButton = gymVideo.querySelector('.gym__video-button');
+  const gymLink = gymVideo.querySelector('.gym__video-link');
+
+  gymLink.removeAttribute('href');
+  gymVideo.classList.add('gym__video-wrapper--enebled');
+
+  gymVideo.addEventListener('click', () => {
+
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.classList.add('gym__video-img');
+    iframe.style.zIndex = '2';
+
+    gymLink.remove();
+    gymButton.remove();
+    gymVideo.appendChild(iframe);
+  });
+
+  const gymPHide = document.querySelector('.gym__wrap--hide');
+
+  const breakpoint = window.matchMedia('(min-width:75em)');
+  const deletePgym = () => {
+    if (breakpoint.matches) {
+      gymPHide.style.display = 'block';
+    } else {
+      gymPHide.style.display = 'none';
+    }
+  };
+  breakpoint.addEventListener('change', deletePgym);
+  deletePgym();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
@@ -34,13 +68,5 @@ window.addEventListener('DOMContentLoaded', () => {
 // url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
 
 // для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
 
 // используйте .closest(el)
