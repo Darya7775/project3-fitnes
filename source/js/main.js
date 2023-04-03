0d12019c60037bf4dfc24788732ae90241d1b544
unknown <stolyarova.199535@gmail.com>
2023-04-03 15:53:17 +0300

    change main

--- source/js/main.js
+++ source/js/main.js
@@ -234,20 +234,3 @@ window.addEventListener('DOMContentLoaded', () => {
     initModals();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-
-// используйте .closest(el)
